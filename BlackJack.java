c4f33c4ee41900ea9615eab917e05dee9d22afa2
kmiono <86765332+kmiono@users.noreply.github.com>
2023-10-11 16:41:04 +0900

    🐛 bug fix

--- BlackJack.java
+++ BlackJack.java
@@ -12,23 +12,41 @@ public class BlackJack {
     public static void main(String[] args) {
         Random rand = new Random();
         try (Scanner scanner = new Scanner(System.in)) {
+
+
             int comCard1 = rand.nextInt(12)+1;//COMに配られるカード1枚目
             int comCard2 = rand.nextInt(12)+1;//COMに配られるカード2枚目
-            //対決前COMに配られるカード2枚目
+            //J、Q、Kは統一で10とする（Aは1とする）
+            if(comCard1 == 11 || comCard1 == 12 || comCard1 == 13){
+                comCard1 = 10;
+            }
+            if(comCard2 == 11 || comCard2 == 12 || comCard2 == 13){
+                comCard2 = 10;
+            }
+
             int comTotal = comCard1+comCard2;
 
 
             int userCard1 = rand.nextInt(12)+1;//プレイヤーに配られるカード1枚目
             int userCard2 = rand.nextInt(12)+1;//プレイヤーに配られるカード1枚目
+
+            //J、Q、Kは統一で10とする（Aは1とする）
+            if(userCard1 == 11 || userCard1 == 12 || userCard1 == 13){
+                comCard1 = 10;
+            }
+            if(userCard2 == 11 || userCard2 == 12 || userCard2 == 13){
+                userCard2 = 10;
+            }
+
             int userTotal = userCard1+userCard2;
             
 
-            String[] trampSuit = {"","A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+            String[] trampSuit = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
 
-            System.out.println("あなたに「"+trampSuit[userCard1]+"」が配られました。");
-            System.out.println("ディーラーに「"+trampSuit[comCard1]+"」が配られました。");
-            System.out.println("あなたに「"+trampSuit[userCard2]+"」が配られました。");
-            System.out.println("ディーラーに「"+trampSuit[comCard2]+"」が配られました。");
+            System.out.println("あなたに「"+trampSuit[userCard1-1]+"」が配られました。");
+            System.out.println("ディーラーに「"+trampSuit[comCard1-1]+"」が配られました。");
+            System.out.println("あなたに「"+trampSuit[userCard2-1]+"」が配られました。");
+            System.out.println("ディーラーに「"+trampSuit[comCard2-1]+"」が配られました。");
 
             System.out.println("ディーラーの合計は "+comTotal+" です。");
             System.out.println("現在の合計は "+userTotal+" です。");
@@ -44,6 +62,12 @@ public class BlackJack {
                     System.out.println("あなたに「"+trampSuit[userBetCard-1]+"」が配られました。");
                     userTotal += userBetCard;
                     System.out.println("現在の合計は "+userTotal+" です。");
+                    // バースト(21以上の失格)の時
+                    if(userTotal > 21){
+                        System.out.println("バーストしたため、あなたの負けです。");
+                        // 処理終了
+                        return;
+                    }
                 }else{
                     break;
                 }
@@ -58,22 +82,21 @@ public class BlackJack {
                     System.out.println("ディーラーに「"+trampSuit[comBetCard]+"」が配られました。");
                     comTotal += comBetCard;
                     System.out.println("ディーラーの合計は "+comTotal+" です。");
+                    // バースト(21以上の失格)の時
+                    if(comTotal > 21){
+                        System.out.println("ディーラーがバーストしたため、あなたの勝ちです。");
+                        // 処理終了
+                        return;
+                    }
                 }else{
                     break;
                 }
             }
 
             //勝敗判定
+            // 処理順：普通に勝った時＞引き分けの時＞普通に負けたとき
             if(userTotal > comTotal){
                 System.out.println("あなたの勝ちです。");
-            }
-            //21を越えた場合はバースト（失格）する
-            else if(userTotal > 21){
-                System.out.println("バーストしたため、あなたの負けです。");
-            }else if(comTotal > 21){
-                System.out.println("ディーラーがバーストしたため、あなたの勝ちです。");
-            }else if(comTotal > 21 && userTotal > 21){
-                System.out.println("双方バーストのため、ノーコンテストです。");
             }else if(comTotal == userTotal){
                 System.out.println("双方同値のため、ノーコンテストです。");
             }
@@ -84,3 +107,31 @@ public class BlackJack {
 
     }
 }
+
+/**
+ * コインをかける場合の処理
+ * 
+ * 
+ * int userCoin = 100;
+ * 
+ * 引き分けの状態も判定する必要があるためint型
+ * int winFlag = 0; 
+ * 
+ * 0になったらゲーム終わる
+ * while(userCoin != 0){
+ * 上記ゲーム処理
+ *   コインの変動
+            if(winFlag == 1){
+                userCoin += 30;
+                System.out.println("あなたのコインは "+userCoin+" です。");
+            }else if(winFlag == 2){
+                userCoin += 20;
+                System.out.println("あなたのコインは "+userCoin+" です。");
+            }else if(winFlag == 0){
+                userCoin -= 10;
+                System.out.println("あなたのコインは "+userCoin+" です。");
+            }
+            }
+ * }
+ * 
+ */
