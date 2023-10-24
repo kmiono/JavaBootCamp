03ae344ac530d037b71abc2ce74c876cdf2b5bfd
kmiono <86765332+kmiono@users.noreply.github.com>
2023-10-24 16:55:23 +0900

    ⚡️ おごり機能追加

--- Kanjikunn.java
+++ Kanjikunn.java
@@ -1,11 +1,17 @@
 public class Kanjikunn {
     public static void main(String[] args) {
-        int totalPrice = 12000;
+        final int gentlemanlyPayment = 500;
+        final int totalPrice = 12000;
         int numberOfMale = 3;
         int numberOfFemale = 3;
+        int malePrice = 0;
+        int femalePrice = 0;
+        
 
-        int warikan = totalPrice / (numberOfMale+numberOfFemale);
-        System.out.println("男性："+warikan+"円");
-        System.out.println("女性："+warikan+"円");
+        int warikanPrice = totalPrice / (numberOfMale+numberOfFemale);
+        malePrice = warikanPrice + gentlemanlyPayment;
+        femalePrice = warikanPrice - gentlemanlyPayment;
+        System.out.println("男性："+malePrice+"円");
+        System.out.println("女性："+femalePrice+"円");
     }
 }
